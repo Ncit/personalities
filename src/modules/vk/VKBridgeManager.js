999fc08657b3ef4a6c213d3828af1d817276db03
ncit <nikmobdev@gmail.com>
2025-08-05 22:27:48 +0300

    - url changed

--- src/modules/vk/VKBridgeManager.js
+++ src/modules/vk/VKBridgeManager.js
@@ -8,7 +8,7 @@ import { LoggerManager } from '../core/LoggerManager.js';
 
 export class VKBridgeManager {
     // Backend API configuration
-    static BACKEND_BASE_URL = 'https://nikmobdev.ru';
+    static BACKEND_BASE_URL = 'https://nikmobdev.ru/goodsshop';
     static BACKEND_CHECK_PURCHASE_ENDPOINT = '/api/check-purchase';
     
     constructor() {
